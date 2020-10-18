7f8d917d896d470041f160b1cda182402bea9fd6
itssudhanshu <sudhanshu_kushwaha@srmuniv.edu.in>
2020-10-18 17:20:16 +0530

    Video 8 Complete

--- lib/Authentication/register.dart
+++ lib/Authentication/register.dart
@@ -15,6 +15,96 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final TextEditingController _nameTextEditingController =
+      TextEditingController();
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
+  final TextEditingController _cPasswordTextEditingController =
+      TextEditingController();
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  String userImageUrl = "";
+  File _imageFile;
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    double _screenWidth = MediaQuery.of(context).size.width,
+        _screenHeight = MediaQuery.of(context).size.height;
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10.0,
+            ),
+            InkWell(
+              onTap: () => print("selected"),
+              child: CircleAvatar(
+                radius: _screenWidth * 0.15,
+                backgroundColor: Colors.white,
+                backgroundImage:
+                    _imageFile == null ? null : FileImage(_imageFile),
+                child: _imageFile == null
+                    ? Icon(
+                        Icons.add_photo_alternate,
+                        size: _screenWidth * 0.15,
+                        color: Colors.grey,
+                      )
+                    : null,
+              ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    CustomTextField(
+                      controller: _nameTextEditingController,
+                      data: Icons.person,
+                      hintText: "Name",
+                      isObsecure: false,
+                    ),
+                    CustomTextField(
+                      controller: _emailTextEditingController,
+                      data: Icons.email,
+                      hintText: "Email",
+                      isObsecure: false,
+                    ),
+                    CustomTextField(
+                      controller: _passwordTextEditingController,
+                      data: Icons.person,
+                      hintText: "Password",
+                      isObsecure: true,
+                    ),
+                    CustomTextField(
+                      controller: _cPasswordTextEditingController,
+                      data: Icons.person,
+                      hintText: "Confirm Password",
+                      isObsecure: true,
+                    ),
+                  ],
+                )),
+            RaisedButton(
+              onPressed: () => ("clicked"),
+              color: Colors.pink,
+              child: Text(
+                "Sign Up",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Container(
+              height: 4.0,
+              width: _screenWidth * 0.8,
+              color: Colors.pink,
+            ),
+            SizedBox(height: 15.0),
+          ],
+        ),
+      ),
+    );
+  }
 }
