056a9fabe449b98210cd9efd713e51053c8f2dd0
itssudhanshu <sudhanshu_kushwaha@srmuniv.edu.in>
2020-10-26 17:15:43 +0530

    old code cart.dart

--- lib/Store/cart.dart
+++ lib/Store/cart.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:grocery/Config/config.dart';
 import 'package:grocery/Address/address.dart';
-import 'package:grocery/Store/product_page.dart';
 import 'package:grocery/Widgets/loadingWidget.dart';
 import 'package:grocery/Models/item.dart';
 import 'package:grocery/Counters/cartitemcounter.dart';
@@ -25,7 +24,6 @@ class _CartPageState extends State<CartPage> {
     Provider.of<TotalAmount>(context, listen: false).displayResult(0);
   }
 
-  int _itemCount = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -151,9 +149,6 @@ class _CartPageState extends State<CartPage> {
                             (context, index) {
                               ItemModel model = ItemModel.fromJson(
                                   snapshot.data.documents[index].data);
-
-                              // model.price = model.price * 2;
-
                               if (index == 0) {
                                 totalAmmount = 0;
                                 totalAmmount = model.price + totalAmmount;
@@ -169,233 +164,9 @@ class _CartPageState extends State<CartPage> {
                                       .displayResult(totalAmmount);
                                 });
                               }
-                              return InkWell(
-                                onTap: () {
-                                  Route route = MaterialPageRoute(
-                                      builder: (c) =>
-                                          ProductPage(itemModel: model));
-                                  Navigator.push(context, route);
-                                },
-                                splashColor: Color(0xff94b941),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    children: [
-                                      Padding(
-                                        padding: EdgeInsets.all(6.0),
-                                        child: Container(
-                                          height: MediaQuery.of(context)
-                                                  .size
-                                                  .height /
-                                              5,
-                                          width: width,
-                                          child: Row(
-                                            children: [
-                                              Padding(
-                                                padding:
-                                                    const EdgeInsets.all(10.0),
-                                                child: Container(
-                                                  decoration: BoxDecoration(
-                                                      color: Colors.white,
-                                                      borderRadius:
-                                                          BorderRadius.all(
-                                                              Radius.circular(
-                                                                  2.0)),
-                                                      boxShadow: [
-                                                        BoxShadow(
-                                                          color: Colors.grey
-                                                              .withOpacity(0.2),
-                                                          spreadRadius: 2,
-                                                          blurRadius: 2,
-                                                          offset: Offset(0,
-                                                              3), // changes position of shadow
-                                                        ),
-                                                      ]),
-                                                  height: MediaQuery.of(context)
-                                                          .size
-                                                          .height /
-                                                      5,
-                                                  child: Image.network(
-                                                    model.thumbnailUrl,
-                                                    width:
-                                                        MediaQuery.of(context)
-                                                                .size
-                                                                .width *
-                                                            0.33,
-                                                    // height: 140.0,
-                                                  ),
-                                                ),
-                                              ),
-                                              SizedBox(
-                                                width: 4.0,
-                                              ),
-                                              Expanded(
-                                                child: Column(
-                                                  crossAxisAlignment:
-                                                      CrossAxisAlignment.start,
-                                                  children: [
-                                                    SizedBox(
-                                                      height: 15.0,
-                                                    ),
-                                                    Container(
-                                                      child: Row(
-                                                        mainAxisSize:
-                                                            MainAxisSize.max,
-                                                        children: [
-                                                          Expanded(
-                                                            child: Text(
-                                                              model.title,
-                                                              style: TextStyle(
-                                                                color: Colors
-                                                                    .black,
-                                                                fontFamily:
-                                                                    "Arial Bold",
-                                                                fontSize: 25.0,
-                                                              ),
-                                                            ),
-                                                          ),
-                                                        ],
-                                                      ),
-                                                    ),
-                                                    SizedBox(height: 5.0),
-                                                    Container(
-                                                      child: Row(
-                                                        mainAxisSize:
-                                                            MainAxisSize.max,
-                                                        children: [
-                                                          Expanded(
-                                                            child: Text(
-                                                              model.shortInfo,
-                                                              style: TextStyle(
-                                                                color: Colors
-                                                                    .black54,
-                                                                fontFamily:
-                                                                    "Arial",
-                                                                fontSize: 20.0,
-                                                              ),
-                                                            ),
-                                                          ),
-                                                        ],
-                                                      ),
-                                                    ),
-                                                    SizedBox(height: 20.0),
-                                                    Row(
-                                                      mainAxisAlignment:
-                                                          MainAxisAlignment
-                                                              .spaceBetween,
-                                                      children: [
-                                                        Padding(
-                                                          padding:
-                                                              EdgeInsets.only(
-                                                                  top: 5.0),
-                                                          child: Row(
-                                                            children: [
-                                                              // Icon(Icons.curr),
-
-                                                              Text(
-                                                                '\u{20B9}${model.price * _itemCount}',
-                                                                style:
-                                                                    TextStyle(
-                                                                  fontSize:
-                                                                      20.0,
-                                                                  color: Colors
-                                                                      .black,
-                                                                ),
-                                                              ),
-                                                              SizedBox(
-                                                                  width: 5.0),
-                                                              Text(
-                                                                '\u{20B9}${model.originalPrice * _itemCount}',
-                                                                style:
-                                                                    TextStyle(
-                                                                  fontSize:
-                                                                      10.0,
-                                                                  color: Colors
-                                                                      .grey,
-                                                                  decoration:
-                                                                      TextDecoration
-                                                                          .lineThrough,
-                                                                ),
-                                                              ),
-                                                            ],
-                                                          ),
-                                                        ),
-                                                       Row(
-                                                                children: [
-                                                                  _itemCount !=
-                                                                          1
-                                                                      ? IconButton(
-                                                                          icon:
-                                                                              Icon(
-                                                                            Icons.remove,
-                                                                            color:
-                                                                                Color(0xff94b941),
-                                                                          ),
-                                                                          onPressed:
-                                                                              () {
-                                                                                setState(() {
-                                                                                  
-                                                                            _itemCount--;
-                                                                                });
-                                                                          }
-                                                                          //  => setState(()=>_itemCount--),
-                                                                          )
-                                                                      : new Container(),
-                                                                  Text(_itemCount
-                                                                      .toString()),
-                                                                  IconButton(
-                                                                      icon:
-                                                                          Icon(
-                                                                        Icons
-                                                                            .add,
-                                                                        color: Color(
-                                                                            0xff94b941),
-                                                                      ),
-                                                                      onPressed:
-                                                                          () {
-                                                                            setState(() {
-                                                                              
-                                                                        _itemCount++;
-                                                                            });
-                                                                      }
-                                                                      // =>setState(()=>_itemCount++))
-                                                                      ),
-                                                                  IconButton(
-                                                                      icon:
-                                                                          Icon(
-                                                                        Icons
-                                                                            .delete,
-                                                                        color: Color(
-                                                                            0xff94b941),
-                                                                      ),
-                                                                      onPressed:
-                                                                          () {
-                                                                        removeItemFromUserCart(model.shortInfo);
-                                                                      }),
-                                                                ],
-                                                              ),
-                                                      ],
-                                                    ),
-                                                    // Divider(height: 5.0, color: Colors.black),
-
-                                                    // Flexible(
-                                                    //   child: Container(),
-                                                    // ),
-                                                  ],
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                      Divider(height: 5.0, color: Colors.grey),
-                                    ],
-                                  ),
-                                ),
-                              );
-                              // sourceInfo(model, context,
-                              //     removeCartFunction: () =>
-                              //         removeItemFromUserCart(model.shortInfo));
+                              return sourceInfo(model, context,
+                                  removeCartFunction: () =>
+                                      removeItemFromUserCart(model.shortInfo));
                             },
                             childCount: snapshot.hasData
                                 ? snapshot.data.documents.length
