657092b93504d84ea154eb214b211685817ac8e3
nirbhaynaruka <49834120+nirbhaynaruka@users.noreply.github.com>
2020-11-16 13:15:43 +0530

    asd
    as

--- lib/Store/category.dart
+++ lib/Store/category.dart
@@ -341,23 +341,23 @@ Widget sourceInfo(ItemModel model, BuildContext context,
                                       child: Center(
                                         child: InkWell(
                                           onTap: () {
-                                            if (logincheck) {
+                                            // if (logincheck) {
                                               checkItemInCart(
                                                   model.shortInfo, context);
-                                            } else {
-                                              Route route = MaterialPageRoute(
-                                                  builder: (_) =>
-                                                      AuthenticScreen());
-                                              Navigator.push(
-                                                context,
-                                                PageRouteBuilder(
-                                                  pageBuilder: (_, __, ___) =>
-                                                      AuthenticScreen(),
-                                                  transitionDuration:
-                                                      Duration(seconds: 0),
-                                                ),
-                                              );
-                                            }
+                                            // } else {
+                                            //   Route route = MaterialPageRoute(
+                                            //       builder: (_) =>
+                                            //           AuthenticScreen());
+                                            //   Navigator.push(
+                                            //     context,
+                                            //     PageRouteBuilder(
+                                            //       pageBuilder: (_, __, ___) =>
+                                            //           AuthenticScreen(),
+                                            //       transitionDuration:
+                                            //           Duration(seconds: 0),
+                                            //     ),
+                                            //   );
+                                            // }
                                           },
                                           child: Container(
                                             decoration: BoxDecoration(
